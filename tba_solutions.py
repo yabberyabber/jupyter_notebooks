a74e22e5b12d9e3b93264a19444af949c081e3b4
Andrew Nelson <andrew2nelson@gmail.com>
2017-10-19 14:19:22 -0700

    Added more problems to tba problemset

--- tba_solutions.py
+++ tba_solutions.py
@@ -22,3 +22,19 @@ def attendance_count(team):
             res[name] = 0
         res[name] += 1
     return res
+
+def winning_alliances(team):
+    res = []
+    for award in tba.team_awards(team):
+        if award['award_type'] == 1:
+            res.append([x['team_key'] for x in award['recipient_list']])
+    return res
+
+def co_win_count(team):
+    res = {}
+    for alliance in winning_alliances(team):
+        for team in alliance:
+            if team not in res:
+                res[team] = 0
+            res[team] += 1
+    return res
